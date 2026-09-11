a2f0316cc0b7f28099c002fd716bf1b605410643
Smith Mbele <ST10472346@rcconnect.edu.za>
2026-09-11 20:48:47 +0200

    created [USER] and successfully executed

--- docs/RaceDay.sql
+++ docs/RaceDay.sql
@@ -2,3 +2,14 @@ CREATE DATABASE RaceDay;
 
 USE RaceDay;
 GO
+
+--Creates User table for creating auth
+--NVARCHAR type to allow characters for strong passwords
+CREATE TABLE [USER] (
+    user_id INT PRIMARY KEY IDENTITY(1,1),
+    username NVARCHAR(100) NOT NULL UNIQUE,
+    password_hash NVARCHAR(255) NOT NULL,
+    email NVARCHAR(150) NOT NULL UNIQUE,
+    role VARCHAR(50) NOT NULL CHECK (role IN ('organiser', 'participant')),
+    created_at DATETIME2 DEFAULT GETUTCDATE()
+    );
