67f84141a008938f1cc4cdc6f01ce82d436ab8a1
Smith Mbele <ST10472346@rcconnect.edu.za>
2026-09-11 20:51:43 +0200

    created APRTICIPANT and ORGANISER, succefully executedthe script

--- docs/RaceDay.sql
+++ docs/RaceDay.sql
@@ -13,3 +13,21 @@ CREATE TABLE [USER] (
     role VARCHAR(50) NOT NULL CHECK (role IN ('organiser', 'participant')),
     created_at DATETIME2 DEFAULT GETUTCDATE()
     );
+
+--Creates Organiser table
+CREATE TABLE ORGANISER (
+    organiser_id INT PRIMARY KEY IDENTITY(1,1),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20),
+    created_at DATETIME2 DEFAULT GETUTCDATE()
+);
+
+--Creates Participant table
+CREATE TABLE PARTICIPANT (
+    participant_id INT PRIMARY KEY IDENTITY(1,1),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20),
+    created_at DATETIME2 DEFAULT GETUTCDATE()
+);
